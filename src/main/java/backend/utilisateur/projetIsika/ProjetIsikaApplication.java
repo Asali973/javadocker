21eec4bba09b3fc5cb238c9496e7937317b6a811
tstho <ts.salmon@proton.me>
2023-10-12 11:47:50 +0200

    pom init

---
+++ src/main/java/backend/utilisateur/projetIsika/ProjetIsikaApplication.java
@@ -0,0 +1,13 @@
+package backend.utilisateur.projetIsika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIsikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIsikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/utilisateur/projetIsika/ProjetIsikaApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.utilisateur.projetIsika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIsikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
